f404b1c645392fb79da4583bad02bec073610872
Treicy <treicy.carolin@gmail.com>
2024-02-11 23:05:55 -0700

    Updating source code

--- main.py
+++ main.py
@@ -4,32 +4,31 @@ class ItemToPurchase:
       self.item_price = 0.0
       self.item_quantity = 0
 
-  def print_item_cost(self):
+  def print_cart_items_cost(self):
       print(f"{self.item_name} {self.item_quantity} @ ${self.item_price} = ${self.item_quantity * self.item_price}")
-
 
 if __name__ == "__main__":
   # Ask the user for the number of items to add
-  num_items = int(input("How many items do you want to add?\n"))
+  number_items = int(input("How many items do you wish to add?\n"))
 
   # Create a list to store the items
   items_list = []
 
   # Loop to get input for each item
-  for i in range(num_items):
+  for i in range(number_items):
       print(f"\nItem {i + 1}")
-      item = ItemToPurchase()
-      item.item_name = input("Enter the item name:\n")
-      item.item_price = float(input("Enter the item price:\n"))
-      item.item_quantity = int(input("Enter the item quantity:\n"))
-      items_list.append(item)
+      cart_item = ItemToPurchase()
+      cart_item.item_name = input("Input the name of the item:\n")
+      cart_item.item_price = float(input("Input the price of the item:\n"))
+      cart_item.item_quantity = int(input("Input the quantity of the item:\n"))
+      items_list.append(cart_item)
 
   # Output the costs of the items
-  print("\nTOTAL COST")
-  total_cost = 0
-  for item in items_list:
-      item.print_item_cost()
-      total_cost += item.item_quantity * item.item_price
+  print("\n***TOTAL COST***")
+  total_amount_cost = 0
+  for cart_item in items_list:
+      cart_item.print_cart_items_cost()
+      total_amount_cost += cart_item.item_quantity * cart_item.item_price
 
   # Output the total cost
-  print(f"\nTotal: ${total_cost}")
+  print(f"\nTotal: ${total_amount_cost}")
